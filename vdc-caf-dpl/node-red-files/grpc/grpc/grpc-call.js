dbd3180d7fab9a40824e0591b115444d8d6e7718
Aitor Fernández <afgomez@ideko.es>
2019-04-30 08:16:02 +0000

    Fixed methode bug

--- vdc-caf-dpl/node-red-files/grpc/grpc/grpc-call.js
+++ vdc-caf-dpl/node-red-files/grpc/grpc/grpc-call.js
@@ -12,10 +12,11 @@ module.exports = function (RED) {
             node.on("input", function (msg) {
                 // overring config with msg content
                 config.service = config.service || msg.service;
-                config.methode = config.methode || msg.methode;
+                config.method = config.method || msg.method;
                 
                 try {
                     const REMOTE_SERVER = serverNode.server + ":" + serverNode.port;
+console.log(REMOTE_SERVER);
                     //Create gRPC client
                     var proto =  serverNode.proto;
                     if (serverNode.protoPackage) {
@@ -24,7 +25,7 @@ module.exports = function (RED) {
                     if (!proto[config.service]) {
                         node.status({fill:"red",shape:"dot",text: "Service " + config.service + " not in proto file"});
                     } else if (!proto[config.service].service[config.method]) {
-                        node.status({fill:"red",shape:"dot",text: "Method " + config.method + " not in proto file for service " +  config.service });
+                        node.status({fill:"red",shape:"dot",text: "Method --" + config.method + "-- not in proto file for service " +  config.service });
                     } else {
                         node.status({});
                         node.client = new proto[config.service](
@@ -35,11 +36,17 @@ module.exports = function (RED) {
 
                     if (node.client) {
                         if (!node.client[config.method]) {
-                            node.status({fill:"red",shape:"dot",text: "Method " + config.method + " not in proto file"});
+                            node.status({fill:"red",shape:"dot",text: "Method >>" + config.method + "<< not in proto file"});
                         } else {
+console.log('Hay cliente y hay metodo');
                             node.status({});
                             if (proto[config.service].service[config.method].responseStream) {
+console.log('Entramos en responsStream');
+// Aitor si hay canal ya sucesivos intentos fallan
+node.channel = null;
+				node
                                 if (!node.channel) {
+console.log('No hay canal');
                                     node.channel = node.client[config.method](msg.payload);
                                     node.channel.on("data", function (data) {
                                         msg.payload = data;
@@ -47,12 +54,19 @@ module.exports = function (RED) {
                                     });
     
                                     node.channel.on("error",function (error) {
+console.log('Error en el canal: ' + error);
+					node.status({fill:"red",shape:"dot",text: "Error de canal: " + error});
                                         msg.error = error;
                                         node.send(msg);
                                     });
                                 }
+				else {
+					console.log('Ya hay canal de antes y no esta implementado mas');
+				}
                             } else {
+console.log("Llamando al metodo...");
                                 node.client[config.method](msg.payload, function(error, data) {
+				    node.status({fill:"red",shape:"dot",text: "Err al llamar al metodo: " + error});
                                     msg.payload = data;
                                     msg.error = error;
                                     node.send(msg);
