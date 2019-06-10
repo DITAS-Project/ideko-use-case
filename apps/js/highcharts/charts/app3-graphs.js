e9e844653a5ab275cd4fcefa8777c1670257c1b0
Borja Tornos <btornos@ideko.es>
2019-06-10 16:29:16 +0200

    improved app3

--- apps/js/highcharts/charts/app3-graphs.js
+++ apps/js/highcharts/charts/app3-graphs.js
@@ -46,7 +46,7 @@ $(document).ready(function() {
 							opacity: 1
 					}
 				},
-				// Quitar el clic en la leyenda para ocultar
+				// Quitar el clic en la leyenda para ocultar 
 				events: {
 					legendItemClick: function() {
 					  return false;
